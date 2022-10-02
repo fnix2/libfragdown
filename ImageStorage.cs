78cb3a6a3c6496bac11f4e364a1620e2a302273e
tihon <tihon111xxx@gmail.com>
2022-10-02 05:13:39 +0300

    Some refactoring in ImageStorage
    Some refactoring in ImageStorage
    
    Some refactoring

--- ImageStorage.cs
+++ ImageStorage.cs
@@ -6,27 +6,40 @@ namespace libfragdown
     {
         private readonly string _pathToImagesDirectory;
 
-        private readonly string _montagedImageName;
+        private readonly string _pathToMontagedImage;
 
-        public ImageStorage(Uri baseUrl, string imageDirParentPath = "/home/tihon/tmp/libfragdown_images/", string montagedImageName = "Montaged.jpg")
+        public ImageStorage(Uri baseUrl, string pathToImagesDirectory, string pathToMontagedImage)
         {
-            using var sha1 = System.Security.Cryptography.SHA1.Create();
-            var urlHash = Convert.ToHexString(sha1.ComputeHash(Encoding.UTF8.GetBytes(baseUrl.ToString())));
-            string fullDirPath = Path.Combine(imageDirParentPath, urlHash);
-            _pathToImagesDirectory = fullDirPath;
-            _montagedImageName = montagedImageName;
-            Directory.CreateDirectory(fullDirPath);
+            string urlHash = GetHashedUrl(baseUrl);
+            _pathToImagesDirectory = Path.Combine(pathToImagesDirectory, "libfragdown", urlHash);
+            _pathToMontagedImage = pathToMontagedImage;
+            Directory.CreateDirectory(_pathToImagesDirectory);
+        }
+
+        public ImageStorage(Uri baseUrl)
+        {
+            string urlHash = GetHashedUrl(baseUrl);
+            _pathToImagesDirectory = Path.Combine(Path.GetTempPath(), "libfragdown", urlHash);
+            _pathToMontagedImage = Path.Combine(_pathToImagesDirectory, "Montaged.jpg");
+            Directory.CreateDirectory(_pathToImagesDirectory);
         }
 
         public string GetMontagedImagePath()
         {
-            return Path.Combine(_pathToImagesDirectory, _montagedImageName);
+            return Path.Combine(_pathToImagesDirectory, _pathToMontagedImage);
         }
 
         public string GetImagePath(ImageCoordinates coordinates)
         {
             string imageName = $"{coordinates.Vertical}_{coordinates.Horizontal}";
             return Path.Combine(_pathToImagesDirectory, imageName);
+        }
+
+        private static string GetHashedUrl(Uri url)
+        {
+            using var sha1 = System.Security.Cryptography.SHA1.Create();
+            var urlHash = Convert.ToHexString(sha1.ComputeHash(Encoding.UTF8.GetBytes(url.ToString())));
+            return urlHash;
         }
     }
 }
